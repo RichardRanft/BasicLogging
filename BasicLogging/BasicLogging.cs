f5cd201c2c6959d0de02add7fb6a6b756ad82c9f
unknown <rranft@WS-2UA2241C65.ad.agi>
2015-06-30 10:52:23 -0700

    Initial commit

---
+++ BasicLogging/BasicLogging.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BasicLogging
+{
+    public class CLog
+    {
+        private String m_logFileName;
+        private StreamWriter m_outputStream;
+
+        public String Filename
+        {
+            get
+            {
+                return m_logFileName;
+            }
+            set
+            {
+                m_logFileName = value;
+                initLogfile();
+            }
+        }
+
+        public CLog ()
+        {
+            m_logFileName = "";
+            m_outputStream = null;
+        }
+
+        private void initLogfile()
+        {
+            if(m_outputStream != null)
+            {
+                m_outputStream.Flush();
+                m_outputStream.Close();
+            }
+            try
+            {
+                if(Directory.Exists(Path.GetDirectoryName(m_logFileName)))
+                    m_outputStream = new StreamWriter(m_logFileName);
+                else
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(m_logFileName));
+                    m_outputStream = new StreamWriter(m_logFileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" -- {0} : Error opening logfile {1} : {2}", DateTime.Now.ToString(), m_logFileName, ex.Message);
+            }
+        }
+
+        public void WriteLine(String line)
+        {
+            if (m_outputStream == null)
+            {
+                initLogfile();
+            }
+
+            try
+            {
+                m_outputStream.WriteLineAsync(String.Format("{0} : {1}",DateTime.Now.ToString(), line));
+                m_outputStream.Flush();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(" -- {0} : Error writing to logfile  {1} : {2}", DateTime.Now.ToString(), m_logFileName, ex.Message);
+            }
+        }
+    }
+}
---
+++ CreatePackage.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+setlocal
+
+cd /d "%~dp0"
+
+set solutionDir=%cd%
+set outDir=%solutionDir%\bin
+
+if exist %outDir% rmdir /s/q %outDir%
+
+
+set config=Release
+set platform=Any CPU
+
+
+echo Building solution 'BasicLogging.sln' (%config%^|%platform%)
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /nologo "/p:SolutionDir=%solutionDir%"\ "/p:Configuration=%config%" "/p:Platform=%platform%" "/p:OutDir=%outDir%"\ /verbosity:minimal "BasicLogging.sln"
+
+if ERRORLEVEL 1 goto :EOF
+
+
+.nuget\nuget pack
+
+rmdir /s/q bin
